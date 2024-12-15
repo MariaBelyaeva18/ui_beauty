ddd2fa18f2239255947c9cc0329e4abd7be9408e
mikhail <shattilov@mail.ru>
2024-12-16 00:25:31 +0300

    new Date

--- client/src/modules/storage/pages/StoragePage.vue
+++ client/src/modules/storage/pages/StoragePage.vue
@@ -115,7 +115,7 @@ const editHandler = (item) => {
     id: item.id,
     materialName: item.name,
     amount: item.amount,
-    expirationDate: item.expirationDate,
+    expirationDate: new Date(item.expirationDate),
   };
 };
 
